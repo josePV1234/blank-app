7b36d9343ed0442b18574af878395c6a7f8a6034
josePV1234 <100118212+josePV1234@users.noreply.github.com>
2026-05-03 12:11:47 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,16 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.set_page_config(page_title="Analizador Pro", page_icon="🎯")
+st.title("🎯 Analizador de Bolsa Directo")
+
+empresa = st.text_input("Introduce la Empresa:", "").upper()
+
+if empresa:
+    st.write(f"### Análisis para {empresa}")
+    resultado = st.radio("Pronóstico de mi IA:", ["Esperando...", "SUBIRÁ", "BAJARÁ"])
+    
+    if resultado == "SUBIRÁ":
+        st.success(f"📈 EL LUNES {empresa} SUBIRÁ")
+        st.balloons()
+    elif resultado == "BAJARÁ":
+        st.error(f"📉 EL LUNES {empresa} BAJARÁ")
